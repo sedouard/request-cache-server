078a6f76550f736f7516d909421a3ae78368be9e
Steven Edouard <sedouard@microsoft.com>
2015-12-31 10:07:38 -0500

    fix(modules/redis_helper) - reuse existing connection

--- modules/redis_helper.js
+++ modules/redis_helper.js
@@ -4,11 +4,14 @@ var conf = require('./config');
 var debug = require('debug')('GitTrack-Cache:redis');
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
-
-
+var redisClient = null;
 var connect = function () {
-  // Connection URL
-  var redisClient = redis.createClient(conf.get('REDIS_URL'));
+
+  if (redisClient && redisClient.connected) {
+    return new Promise(resolve => resolve(redisClient));
+  }
+
+  redisClient = redis.createClient(conf.get('REDIS_URL'));
 
   return new Promise((resolve, reject) => {
     redisClient.on('connect', () => {
@@ -17,6 +20,7 @@ var connect = function () {
     });
     redisClient.on('error', (err) => {
       debug('Error connecting to redis');
+      console.error(err);
       return reject(err);
     });
   });
